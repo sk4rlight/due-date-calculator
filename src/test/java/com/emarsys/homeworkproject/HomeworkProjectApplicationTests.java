0b6f48af8639552182ec65ece4949d4a0ed95512
Phan Xuan Phuong <pxphuong94@gmail.com>
2021-05-28 12:05:53 +0200

    Initial project commit

---
+++ src/test/java/com/emarsys/homeworkproject/HomeworkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emarsys.homeworkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
